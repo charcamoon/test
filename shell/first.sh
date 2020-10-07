321afdd3728e77c5b7c95237cb1de790810ac49d
charcamoon <myiuni@126.com>
2020-10-07 15:02:45 +0800

    This is my first shell script

---
+++ shell/first.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Author:me
+#Blog:https://blog.me.com/none
+#Time:2020-10-07 14:59:10
+#Name:first.sh
+#Version:V1.0
+#Description:This is a production script.
+
+#This is to show what a example looks like
+
+echo "Out first example."
+
+echo #this inserts an expty line in output.
+
+echo "We are currently in the following diractory."
+
+echo ""
+
+/bin/pwd
+
+echo
+
+echo "This diractory contains following files"
+
+/bin/ls
+
